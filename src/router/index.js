dbc560658d812175e75255391ce7c80a13bd1004
pream912 <preamkumar912@gmail.com>
2020-12-26 01:26:38 +0530

    first commit

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Client from '../views/Client.vue'
+import Main from '../views/Main.vue'
 
 Vue.use(VueRouter)
 
@@ -11,12 +13,12 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/main',
+    component: Main
+  },
+  {
+    path: '/user/:id',
+    component: Client
   }
 ]
 
--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,9 +1,19 @@
 <template>
-    <div width="1920" height="1080">
-        <v-row align="center" justify="center">
-            <video height="720" ref="fvdo" autoplay muted playsinline></video>
+    <v-container>
+        <v-row>
+            <v-col cols="6">
+                <video ref="mvdo" autoplay muted playsinline></video>
+            </v-col>
+            <v-col cols="6">
+                <video ref="fvdo" autoplay playsinline></video>
+            </v-col>
+            <v-col>
+                <v-btn @click="showFriendsFace"> Call</v-btn>
+                <v-btn @click="shareScreen"> Screen</v-btn>
+                <v-btn @click="showMyFace"> switch back</v-btn>
+            </v-col>
         </v-row>
-    </div>
+    </v-container>
 </template>
 
 <script>
@@ -12,7 +22,7 @@ import 'firebase/database'
 export default {
     data: () => ({
         database: firebase.database().ref(),
-        yourId: '1000',
+        yourId: '1001',
         pc: new RTCPeerConnection({'iceServers': [{'urls': 'turn:68.183.92.240:3478','credential': 'Kumar912','username': 'pream912'}]})
     }),
 
@@ -26,7 +36,7 @@ export default {
         readMessage(data) {
             var msg = JSON.parse(data.val().message)
             var sender = data.val().sender
-            if (sender == '1001') {
+            if (sender != this.yourId) {
                 if (msg.ice != undefined)
                     this.pc.addIceCandidate(new RTCIceCandidate(msg.ice));
                 else if (msg.sdp.type == "offer")
@@ -39,6 +49,18 @@ export default {
             }
         },
 
+        showMyFace() {
+            navigator.mediaDevices.getUserMedia({audio:true, video:true})
+            .then(stream => this.$refs.mvdo.srcObject = stream)
+            .then(stream => this.pc.addStream(stream))
+        },
+
+        shareScreen () {
+            navigator.mediaDevices.getDisplayMedia()
+            .then(stream => this.$refs.mvdo.srcObject = stream)
+            .then(stream => this.pc.addStream(stream))
+            .then(() => this.showFriendsFace())
+        },
 
         showFriendsFace() {
             this.pc.createOffer()
@@ -51,6 +73,7 @@ export default {
         this.pc.onicecandidate = (event => event.candidate?this.sendMessage(this.yourId, JSON.stringify({'ice': event.candidate})):console.log("Sent All Ice") )
         this.pc.onaddstream = (event => this.$refs.fvdo.srcObject = event.stream)
         this.database.on('child_added', this.readMessage)
+        this.showMyFace()
     }
 }
 </script>
